b877304000e82dca8c69ba311b18f4d0e6951429
Wilton Mendes <134981092+wiltonfmendes@users.noreply.github.com>
2024-08-13 14:10:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const mensagemFinal = document.getElementById('mensagemFinal');
     const container = document.querySelector('.container');
 
-    // Defina o volume da música (0.3 = 30% do volume) para dispositivos móveis
+
     musica.volume = 0.3;
 
     naoButton.addEventListener('mousemove', () => {
@@ -21,18 +21,18 @@ document.addEventListener('DOMContentLoaded', () => {
         naoButton.style.left = `${x}px`;
         naoButton.style.top = `${y}px`;
 
-        // Mostrar o botão "Sim" ao primeiro movimento do botão "Não"
+   
         simButton.style.display = 'block';
     });
 
     simButton.addEventListener('click', () => {
-        // Tocar a música
+        
         musica.play();
 
-        // Exibir a mensagem
+       
         mensagemFinal.style.display = 'block';
 
-        // Desativar os botões para evitar interação adicional
+        
         simButton.disabled = true;
         naoButton.disabled = true;
     });
